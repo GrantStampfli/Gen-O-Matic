e10a5431500ec657cb0533a1c9ed6bc84926957e
Ariel Spear <simpleredfox@gmail.com>
2014-06-02 17:13:33 -0700

    Changed some file stuff, made tests better

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 'use strict';
+var fs = require('fs');
 
 var readFileNAme = module.exports.readFileName = function() {
+  var dir = ('./pages');
 
+  //var result = [];
+    var result = fs.readdir(dir, result);
+  return result;
+};
+
+var countFiles = module.exports.countFiles = function() {
+  var result;
+
+  return result;
 };
--- test/test.js
+++ test/test.js
@@ -1,10 +1,20 @@
 var expect = require('chai').expect;
-var _ = require('..');
+var path = require('path');
+var lib = require('..');
 
 describe('readFileName()', function() {
 
-  it('should read the file names of the pages directory', function(){
-    expect(_.readFileName()).to.eql(['about.html', 'home.html']);
+  it.only('should read the file names of the pages directory', function(){
+    var site1Path = path.join(__dirname, 'fixtures/site1');
+    expect(lib.readFileName(site1Path)).to.eql(['about.html', 'home.html']);
+  });
+
+});
+
+describe('countFiles()', function(){
+
+  it('should tell us how many files are in the pages directory', function(){
+    expect(lib.countFiles()).to.eql(2);
   });
 
 });
