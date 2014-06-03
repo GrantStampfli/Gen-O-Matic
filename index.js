2887ef5e7b3ad97a1d8bd2761e7b14d6ced3f87e
Grant Stampfli <mail@gstampfli.com>
2014-06-03 12:04:35 -0700

    fixed test two to count files

--- index.js
+++ index.js
@@ -2,7 +2,15 @@
 var fs = require('fs');
 var path = require('path');
 
-
+/**
+ * Read file names
+ *
+ * @function
+ * @param {string} sitePath - The file path to a site directory.
+ * @param {function} cb - The callback function which takes two args, 
+ * those two are `err` and `files` `err` is an error and `files`
+ * is an array of strings containing the names of the files.
+ */
 var readFileName = module.exports.readFileName = function(sitePath, cb) {
   var dir = path.join(sitePath, 'pages');
   fs.readdir(dir, function(err, files){
@@ -10,9 +18,17 @@ var readFileName = module.exports.readFileName = function(sitePath, cb) {
   });
 };
 
+/**
+ * Count files
+ *
+ * @function
+ * @param {string} sitePath - The file path to a site directory.
+ * @param {function} cb - The callback function which takes two args, 
+ * those two are `err` and `count`, `err` is an error and `count`
+ * is a number, represents the number of files.
+ */
 var countFiles = module.exports.countFiles = function(sitePath, cb) {
-  
-  var result;
-  readFileName(sitePath, cb);
-  return result;
+	readFileName(sitePath, function(err, files){
+		cb(err, files.length);
+	});
 };
--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var path = require('path');
 var lib = require('..');
 
 describe('readFileName()', function(){
-
+// 
   it('reads the file names of the site1/pages', function(done){
     var site1Path = path.join(__dirname, 'fixtures/site1');
     lib.readFileName(site1Path, function(err, result){
@@ -19,7 +19,7 @@ describe('countFiles()', function(){
   it('should tell us how many files are in site1/pages', function(done){
     var site1Path = path.join(__dirname, 'fixtures/site1');
     lib.countFiles(site1Path, function(err, result){
-      expect(result.length).to.eql(2);
+      expect(result).to.eql(2);
       done();
     });
   });
